3eb827e78c2d5bf79541be980294117a4dec89e2
AlessandraGolini <146853793+AlessandraGolini@users.noreply.github.com>
2023-10-03 17:04:01 +0200

    Create 01.beginning.R

---
+++ 01.beginning.R
@@ -0,0 +1,19 @@
+# Here I can write anything I want
+# part of the exam is actually manner of how much you comment your code because the comments help to understand what the code is running.
+
+# R as a calculator
+2+3
+
+# Assign to an object
+zima <- 2+3
+
+duccio <- 5+3
+duccio
+
+zima*duccio
+
+final <- zima*duccio
+final
+
+final^2
+
