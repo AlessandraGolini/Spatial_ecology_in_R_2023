f50e32293497887b8dbfc1f8022d6c433fbb01e3
AlessandraGolini <146853793+AlessandraGolini@users.noreply.github.com>
2023-10-19 17:14:55 +0200

    Update and rename 03.multivar21.R to 03.communities.multivar21.R

--- 03.communities.multivar21.R
+++ 03.communities.multivar21.R
@@ -8,19 +8,24 @@ tail(dune) #you can see the final part of the dataset
 
 ord <- decorana(dune) #detrended correspondence analysis
 
-ldc1 <- 3.7004
-ldc2 <- 3.2266
-ldc3 <- 1.30055
-ldc4 <- 1.47888
 
-total = ldc1 + ldc2 + ldc3 +lcd4
+ldc1 =  3.7004 
+ldc2 =  3.1166 
+ldc3 = 1.30055
+ldc4 = 1.47888
 
-plc1 = ldc1/total * 100
-plc2 = ldc2/total * 100
-plc3 = ldc3/total * 100
-plc4 = ldc4/total * 100
-#??? mi sono persa boia!!!
+total = ldc1 + ldc2 + ldc3 + ldc4
 
+pldc1 = ldc1 * 100 / total
+pldc2 = ldc2 * 100 / total
+pldc3 = ldc3 * 100 / total
+pldc4 = ldc4 * 100 / total
 
+pldc1
+pldc2
+
+pldc1 + pldc2
+
+plot(ord) 
 
 #from the table it is almost impossible to understand the corrispondence between the species, therefort I use a plot with only 2 axis
