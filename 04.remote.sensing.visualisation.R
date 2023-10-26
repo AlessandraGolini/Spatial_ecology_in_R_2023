553de805cf40926413e0f98295667a314ffa4a6b
AlessandraGolini <146853793+AlessandraGolini@users.noreply.github.com>
2023-10-26 17:42:56 +0200

    Update 04.remote.sensing.visualisation.R

--- 04.remote.sensing.visualisation.R
+++ 04.remote.sensing.visualisation.R
@@ -10,6 +10,18 @@ devtools::install_github("ducciorocchini/imageRy") # install imagery
 library(imageRy) # to make a check
 
 im.list()
+# band number 2 on the list:
+b2 <- im.import("sentinel.dolomites.b2.tif") 
+b2
 
-im.import("sentinel.dolomites.b2.tif") 
-#non mi viene:(
+#let's change the color
+clb <- colorRampPalette(c("dark grey", "grey", "light grey")) (100)
+plot(b2, col=clb)
+
+# import the green band from Sentinel-2 (band 3)
+b3 <- im.import("sentinel.dolomites.b3.tif")
+b3
+
+clr <- colorRampPalette(c("red","pink","light pink")) (100)
+plot(b3, col=clr)
+#everithing that is light in the picture is reflecting the (green) light, while everything that is dark in the picture is absorbing it.
