7d9514ecd34c2b58d338e260596862842299a879
AlessandraGolini <146853793+AlessandraGolini@users.noreply.github.com>
2023-11-02 18:02:59 +0100

    Update 06.remote.sensing.data.visualisation.R

--- 06.remote.sensing.data.visualisation.R
+++ 06.remote.sensing.data.visualisation.R
@@ -49,18 +49,36 @@ plot(b4, col = cl)
 b8 <- im.import("sentinel.dolomites.b8.tif")
 plot(b8, col = cl)
 
+
+par(mfrow = c(2,2))
+plot(b2, col = cl)
+plot(b3, col = cl)
+plot(b4, col = cl)
+plot(b8, col = cl)
+dev.off() # it closes the devices
+
+# stack images all together
+stacksent <- c(b2, b3, b4, b8)
+
+plot(stacksent, col = cl)
+
+# to plot just one layer from the stack we can use the number of the "element" (layer)
+plot(stacksent[[4]], col = cl)
+
 # Let's put all the images in a single graph
-# EXERCISE: plot in a multiframe the bands with different color maps
+# EXERCISE: Plot in a multiframe the bands with different color ramps
 par(mfrow=c(2,2))
 
-clb <- colorRampPalette(c("dark blue", "blue", light blue")) (100)
-plot(b2, col=clb)
-clg <- colorRampPalette(c("dark green", "green", light green")) (100)
-plot(b3, col=clg)
-clr <- colorRampPalette(c("dark red", "red", "pink")) (100)
-plot(b4, col=clr)
-cln <- colorRampPalette(c("brown", "orange", yellow")) (100)
-plot(b8, col=cln)
+cl2 <- colorRampPalette(c("dark blue", "blue", "light blue")) (100)
+cl3 <- colorRampPalette(c("dark green", "green", "light green")) (100)
+cl4 <- colorRampPalette(c("firebrick4", "firebrick", "red")) (100)
+cl8 <- colorRampPalette(c("orangered", "gold", "yellow")) (100)
+
+par(mfrow = c(2,2))
+plot(b2, col = cl2)
+plot(b3, col = cl3)
+plot(b4, col = cl4)
+plot(b8, col = cl8)
 
 
 ############################################################
