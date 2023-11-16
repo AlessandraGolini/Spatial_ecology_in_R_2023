dba2b6f03bbf960d5fb39f0be6918e1f046dd0a3
AlessandraGolini <146853793+AlessandraGolini@users.noreply.github.com>
2023-11-16 17:44:17 +0100

    Update 07.external.data.code.R

--- 07.external.data.code.R
+++ 07.external.data.code.R
@@ -8,5 +8,37 @@ setwd("C:/Users/aless/OneDrive - Alma Mater Studiorum Università di Bologna/Glo
 
 # let's import the data with a new function from terra: rast(name of the image), like in im.import()
 naja <- rast("najafiraq_etm_2003140_lrg.jpg")
-
 plotRGB(naja, r=1, g=2, b=3)
+
+# Exercise: download the second image from the same site and import it into R
+najaaug <- rast("najafiraq_oli_2023219_lrg.jpg")
+plotRGB(najaaug, r=1, g=2, b=3)
+
+#plot the two images together
+par(mfrow=c(2,1))
+plotRGB(naja, r=1, g=2, b=3)
+plotRGB(najaaug, r=1, g=2, b=3)
+
+# multitemporal change detection
+## we're making the difference between the naja[[1]] - najaaug[[1]]
+najadif = naja[[1]] - najaaug[[1]]
+cl <- colorRampPalette(c('brown', 'grey', 'orange')) (100)
+plot(najadif, col = cl)
+
+# download your own image
+flood <- rast("italyflooding_oil_2023307_lrg.jpg")
+plotRGB(flood, r=1, g=2, b=3)
+
+#I can also switch the bands in different compositions:
+plotRGB(flood, r=2, g=1, b=3)
+plotRGB(flood, r=3, g=2, b=1)
+
+#example of mato grosso from earth observatory (NASA):
+mato <- rast("matogrosso_l5_1992219_lrg.jpg")
+plotRGB(mato, r=1, g=2, b=3) #the near infrared is on top of the red layer
+
+
+## COPERNICUS
+#ncdf4: Interface to Unidata netCDF (Version 4 or Earlier) Format Data Files
+install.packages("ncdf4")
+library(ncdf4)
