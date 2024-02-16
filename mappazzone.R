01643a82ee10d279ba5dde7137e01be76d831f32
AlessandraGolini <146853793+AlessandraGolini@users.noreply.github.com>
2024-02-16 22:47:40 +0100

    Create mappazzone.R

---
+++ mappazzone.R
@@ -0,0 +1,3 @@
+#pch = point character https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6gmJapH_j6E3zWbTCOuDKJDUrH80VvTYdgCNVMh16OA&s
+#cex = character exageration, it defines the dimensions of the points
+#col = color
