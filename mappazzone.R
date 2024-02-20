b8b52e775e3233193a8667792d245af9747e5034
AlessandraGolini <146853793+AlessandraGolini@users.noreply.github.com>
2024-02-20 17:32:29 +0100

    Delete mappazzone.R

--- mappazzone.R
+++
@@ -1,3 +0,0 @@
-#pch = point character https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6gmJapH_j6E3zWbTCOuDKJDUrH80VvTYdgCNVMh16OA&s
-#cex = character exageration, it defines the dimensions of the points
-#col = color
